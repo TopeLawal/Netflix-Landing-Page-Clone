9ea722f5c0f747044b1c066e00dd241dc40823ce
Obinna <temitopelaw1@gmail.com>
2023-03-25 21:44:55 +0100

    some styling on the faq section

---
+++ main.js/main.js
@@ -0,0 +1,8 @@
+const accordion = document.querySelectorAll('.accordion');
+
+for ( i = 0; i < accordion.length; i++) {
+    
+    accordion[i].addEventListener('click', function () {
+        this.classList.toggle('active');
+    })
+}
